1cdd32a8eed889a714c625dc3e4c1303f50f397e
AN <99376802+alainatividad@users.noreply.github.com>
2022-04-30 21:25:33 +1000

    Added the HTML base into index.js and added fs method to write the index.html

--- index.js
+++ index.js
@@ -1,20 +1,51 @@
+// include modules for filesystem and inquirer
+const fs = require("fs");
+const inquirer = require("inquirer");
+
 // include class definitions
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const questions = require("./lib/questions");
-// include modules for filesystem and inquirer
-const fs = require('fs');
-const inquirer = require("inquirer");
+const returnArrayQuestion = require("./lib/questions");
+// const generatePage = require("./lib/generatePage");
+const generateHTML = require("./lib/generateHTML");
 
 // let's store the input objects into an array
 let team = [];
+let question = [];
+const generatePage = (cardsHTML) =>
+  `<!DOCTYPE html>
+<html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+      <title>My Team</title>
+  </head>
+  <body>
+      <header class="jumbotron jumbotron-fluid text-center bg-dark">
+          <div class="container-fluid">
+              <p class="h1 text-white">My Team</p>
+          </div>
+      </header>
+      <div class="container">
+          <div class="row row-cols-2 row-cols-md-4">
+          ${cardsHTML}  
+          </div>
+      </div>
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  </body>
+</html>`;
 
 function start() {
+  question = returnArrayQuestion("Team Manager");
   // the program starts with asking for the manager's details
   inquirer
     .prompt([
-      ...questions,
+      ...question,
       {
         type: "input",
         name: "officeNum",
@@ -33,21 +64,21 @@ function start() {
       manager.getName(val.name);
       manager.getEmail(val.email);
       manager.getId(val.empId);
-      manager.getNumber(val.officeNumber);
+      manager.getNumber(val.officeNum);
 
       // add it to the empty team array
       team.push(manager);
-      console.log(JSON.stringify(team));
       // show prompt to add members or finish creating the team
       addMore();
     });
 }
 
 function getEngineer() {
+  question = returnArrayQuestion("Engineer");
   // if the user selects Engineer from the prompt, ask Engineer's details
   inquirer
     .prompt([
-      ...questions,
+      ...question,
       {
         type: "input",
         name: "github",
@@ -70,17 +101,17 @@ function getEngineer() {
 
       // push new Engineer to team
       team.push(engineer);
-      console.log(team);
       // show prompt to add members or finish creating the team
       addMore();
     });
 }
 
 function getIntern() {
+  question = returnArrayQuestion("Intern");
   // When Intern is selected on the addMore prompt, ask Intern details
   inquirer
     .prompt([
-      ...questions,
+      ...question,
       {
         type: "input",
         name: "school",
@@ -103,7 +134,6 @@ function getIntern() {
 
       // add new intern to team
       team.push(intern);
-      console.log(team);
       // show prompt to add members or finish creating the team
       addMore();
     });
@@ -137,12 +167,17 @@ function addMore() {
 }
 
 function quit() {
-  let teamHTML = '';
-  team.forEach(teamMember) {
+  let teamHTML = "";
+
+  // generate card for each team member
+  team.forEach((teamMember) => {
     teamHTML += generateHTML(teamMember);
-  }
-  
-  process.exit(0);
+  });
+
+  //append the created cards to the base HTML and then write to index.html
+  fs.writeFile("./dist/index.html", generatePage(teamHTML), (err) =>
+    err ? console.error(err) : console.log("index.html successfully created!")
+  );
 }
 
 // Function call to initialise app
